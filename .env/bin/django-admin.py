35eecca0e34a506afee9b3ba59896f6deca162b1
Thomas Ferullo <thomasferullo@Andrews-MacBook-Pro.local>
2018-09-10 15:25:58 -0400

    added movies model and got it to render

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/thomasferullo/dev/wdi/projects/project-4-film-critic/Film-Critic/.env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
