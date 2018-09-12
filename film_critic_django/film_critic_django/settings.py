1846d389fe7961c780fe193923f75b316877991e
Thomas Ferullo <thomasferullo@Andrews-MacBook-Pro.local>
2018-09-12 11:13:12 -0400

    depoying commit

--- film_critic_django/film_critic_django/settings.py
+++ film_critic_django/film_critic_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x=h23$3m9s(rt73l9da+w$!z)u!^^keezupt8)o%sa&w^^j-**'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [‘0.0.0.0’, ‘localhost’, ’https://film-critic.herokuapp.com/’]
+ALLOWED_HOSTS = [ ‘localhost’, ’https://film-critic.herokuapp.com/’]
 
 
 # Application definition
